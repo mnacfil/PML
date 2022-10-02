251af48f2bb0e7f8b76189eda716be2fdd04ab20
Melvin Nacfil <mnacfil@gmail.com>
2022-10-03 01:37:14 +0800

    Finish Aggregation of most used item

--- controllers/orderController.js
+++ controllers/orderController.js
@@ -1,10 +1,12 @@
 const Order = require('../model/Order');
 
+// Post request to save the order in DB
 const createOrder = async (req, res) => {
     const order = await Order.create(req.body);
     res.status(201).json({message: 'Order saved!'});
 }
 
+// Get All Orders, with Query
 const getOrders = async (req, res) => {
     const { size, type, crust } = req.query;
     let query = {};
@@ -56,7 +58,21 @@ const getOrders = async (req, res) => {
     res.status(200).json({orders, count: orders.length})
 }
 
+// aggregrate all the topping used in custom
+const toppingStats = async (req, res) => {
+    let stats = await Order.aggregate([
+        // 1st stage, 
+        { $unwind: "$pizzas"},
+        { $unwind: "$pizzas.toppings"},
+        { $unwind: "$pizzas.toppings.toppingItem"},
+        // 2nd stage, group base on topping item
+        { $group: { _id: '$pizzas.toppings.toppingItem.item', totalItem: { $sum: 1 }} },
+    ])
+    res.status(200).json({stats});
+}
+
 module.exports = {
     createOrder,
-    getOrders
+    getOrders,
+    toppingStats
 }
--- routes/orderRoute.js
+++ routes/orderRoute.js
@@ -2,9 +2,10 @@ const express = require('express');
 const router = express.Router();
 
 // controllers
-const { createOrder, getOrders} = require('../controllers/orderController')
+const { createOrder, getOrders, toppingStats} = require('../controllers/orderController')
 
 router.post('/create-order', createOrder);
 router.get('/get-orders', getOrders);
+router.get('/topping-stats', toppingStats);
 
 module.exports = router;
